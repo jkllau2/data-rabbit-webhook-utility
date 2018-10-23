eddd8d7019d786590ec5f8c024b6a8bf7f2510cc
Justin Lau <justin@justinlau.name>
2018-10-23 17:09:25 -0400

    refactor: Cleaned up server.js

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Middleware
-const bodyParser = require('body-parser');
-const boolParser = require('express-query-boolean');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-
 // Utilieis
 const logger = require('./utils/loggerUtils');
 const portNormalizer = require('./utils/portNormalizer');
 
 logger.log('Starting API server', 'ready');
 logger.log(`Running Node version ${process.version}`, 'ready');
-
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
-app.use(cookieParser());
-app.use(boolParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-app.use(require('morgan')('combined'));
-app.use(require('compression')());
 
 app.set('port', portNormalizer.normalizePort(process.env.PORT || '3333'));
 
